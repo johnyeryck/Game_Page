7709eb8430909d8dfe868e18c1424a394ad2b23b
Johnyeryck <johnybombado@gmail.com>
2025-04-09 21:31:09 -0300

    login user

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,10 +4,23 @@ import Carrinho from '../assets/logo/carrinho-de-compras.png'
 import {Link } from 'react-router-dom'
 import Usuario from '../assets/logo/usuario.png'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
 function Nav() {
+    const {data} = useQuery({
+        queryKey : ["data"],
+        queryFn : async () => {
+        const res = await fetch('http://localhost:3000')
+        const result = await res.json()
+        if(result[0].user){
+            setlogin(true)
+        }
+        return result
+        
+}})  
+    console.log(data)
     const [isLoged ,setlogin] = useState <Boolean>(false)
     const [isVisible, setVisible] = useState <Boolean>(false)
-    const [user ,setUser]= useState<string>("Algostinho")
     
     
 
@@ -25,27 +38,30 @@ function Nav() {
             <Link to="/cart" className='size-8 ml-auto mt-11 mr-3 cursor-pointer '>
                 <img src={Carrinho}  alt="Shopping Cart"/>
             </Link>
-            <div onMouseEnter={()=> setVisible(true)} onMouseLeave={()=> setVisible(false)} className="relative group">
+            <div>
                 {
-                    isLoged &&(
-                        <div className='flex mr-3 '>
+                    isLoged == true ? (
+                        <div className='flex mr-3 ' onMouseEnter={()=> setVisible(true)} onMouseLeave={()=> setVisible(false)} >
                             <img className='size-11 mt-9   cursor-pointer ' src={Usuario}></img>
-                            <p className='text-white mt-12 font-bold'>{user}</p>
+                            <p className='text-white mt-12 font-bold'>{data[0].user}</p>
+                            {
+                            isVisible && (
+                                <ul className="absolute top-20 group-hover:block bg-gray-300 text-black shadow-lg rounded-md w-24 font-semibold">
+                                    <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">Perfil</li>
+                                    <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer ">Config</li>
+                                    <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">Sair</li>                        
+                                </ul>
+                            
+                            )
+                            }
                         </div>
 
+                    ) : (
+                        <div className='flex mr-3 mt-12'>
+                            <Link to="/login" className='flex mr-3 text-white '>Entrar    | </Link> 
+                            <Link to="/createAccount" className='flex mr-3 text-white'>Registrar</Link>
+                        </div>
                     )
-                }
-                {
-                isVisible && (
-                    <ul className="absolute left-0 group-hover:block bg-gray-300 text-black shadow-lg rounded-md w-24 font-semibold">
-                        <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">Perfil</li>
-                        <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer ">Config</li>
-                        <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer" onClick={()=>{
-                            setUser("")
-                        }}>Sair</li>                        
-                    </ul>
-
-                )
                 }
             </div>
         </nav>

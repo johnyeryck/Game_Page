8479274400252d05618f7a22190302e61272e26e
Johnyeryck <johnybombado@gmail.com>
2025-04-22 15:02:11 -0300

    .

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const {Poll} = require('pg') 
-const Poll = new Poll({
-    host : 'localhost',
-    port : 5432,
-    user : 'johny',
-    password : '1234',
-    database : 'localhost'
-})
-module.exports = Poll

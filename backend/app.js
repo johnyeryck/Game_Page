b899c006ac71d1bd0e982f515a95c37009918dcb
Johnyeryck <johnybombado@gmail.com>
2025-05-01 23:07:10 -0300

    config login authentication

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import morgan from 'morgan';
 import cors from 'cors';
 import pool from './db.js';
 const app = express()
-
+app.use(express.json())
 // Configuração do CORS
 app.use(cors({
    origin: 'http://localhost:5173',
@@ -13,16 +12,12 @@ app.use(cors({
    credentials: true
 }));
 
-app.use(express.json())
-
-let data = []
 // Middleware para registrar os dados recebidos
-app.post('/',async (req , res)=>{
-   data = req.body
+app.post('cadastros/',async (req , res)=>{
    const { email, senha, user } = req.body
-
+   
    try{
-    await pool.query( 'INSERT INTO usuarios (email,senhar,usuario) VALUES ($1,$2,$3) RETURNING *', [email,senha,user])
+      await pool.query( 'INSERT INTO usuarios (email,senhar,usuario) VALUES ($1,$2,$3) RETURNING *', [email,senha,user])
    }
    catch(err){
       return res.status(500).json({
@@ -31,19 +26,22 @@ app.post('/',async (req , res)=>{
          stack: err.stack,    
       });
    }
-
+   
    res.status(200).json({
       message: 'Dados recebidos com sucesso',
       data: data
-  
-})})
-
-app.get('/',async(req,res)=>{
-   res.status(200).json(data);
-   console.log(req.method)
-   if(req.method === 'GET'){
-     const result = await pool.query('SELECT * FROM usuarios WHERE email = $1 AND senhar = $2', [data.email,data.senha])
-     console.log(result.rows)
-   }
-})
+      
+   })})
+   
+   
+   app.post('/login',async(req,res)=>{
+      const { email, senha } = req.body
+   if(req.method === 'POST'){
+     const result = await pool.query('SELECT * FROM usuarios WHERE email = $1 AND senhar = $2', [email,senha]);
+       if(result.rows.length > 0){
+         res.status(200).json({
+             message: 'Login bem-sucedido',
+             data: result.rows
+         })
+   }}})
 export default app

afee23528652fc2c689895ed73d280a93ac1688d
johnyeryck <johnybombado@gmail.com>
2025-05-30 18:54:23 -0300

    feat(App): integrate Genders component and set up context for game data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import Nav from "./components/Nav";
 import Main from "./components/slide";
-import Layout from "./components/layout"
-function App() {
+import Layout from "./components/acao"
+import Rpg from "./components/rpg";
+import DataGames from "./interfaces";
+import { useQuery } from "@tanstack/react-query";
+import MyContext from "./context/gamesContext";
+import Genders from "./components/genders";
 
+
+function App() {
+  const { data} = useQuery<DataGames[] | undefined>({
+    queryKey: ['games'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3000/games', {
+        method : "GET"
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      
+      return response.json()
+    }
+  })
   
   return (
     <>
-      <Nav StyleNav="w-full bg-black h-20 flex z-10 top-0 fixed"/>
-      <Main />
-      <Layout />
+      <MyContext.Provider value={data}>
+          <Nav />
+          <Main />
+          <Layout />
+          <Genders />
+          <Rpg />
+      </MyContext.Provider>
     </>
   );
 }

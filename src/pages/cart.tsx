f0669de8be712fc80d53cc0cd765aef46358df92
johnyeryck <johnybombado@gmail.com>
2025-06-02 19:05:04 -0300

    fix(Cart): conditionally render Options component based on login status

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,10 +1,19 @@
 import Nav from "@/components/Nav";
+import Options from "@/components/options";
 
 function Cart() {
+
+  let isLoged = localStorage.getItem("isLoged")
   return (
       
     <div>
       <Nav/>
+      {
+        isLoged == "true" ? (
+          <Options imageUrl="" />
+
+        ) : null
+      }
     </div>
       
     

7123a29e060b5315108fc8185d34925c5c16b195
johnyeryck <johnybombado@gmail.com>
2025-06-02 19:04:34 -0300

    fix(Options): remove unnecessary whitespace in button component

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -11,7 +11,7 @@ const  Options : FunctionComponent<CartInterface>=({imageUrl , gameID})=> {
       return(
           <div className="h-60  w-48 transform transition-all hover:scale-110 rounded-xl  ">
               <img className=" rounded-2xl size-full" src={imageUrl}/>
-              <button className="absolute bottom-4 right-4 bg-white p-2 rounded-full size-8 cursor-pointer  " onClick={()=>CartFunction()} ><img src={Carrinho} alt="Adicionar ao carrinho" /></button>
+              <button className="absolute bottom-4 right-4 bg-white p-2 rounded-full size-8 cursor-pointer"  onClick={()=>CartFunction()} ><img src={Carrinho} alt="Adicionar ao carrinho" /></button>
           </div>
       )
 }

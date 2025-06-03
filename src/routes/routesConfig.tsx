f4aa7edf32b8e49344e0189300239ccc88bf53d8
johnyeryck <johnybombado@gmail.com>
2025-06-02 21:53:14 -0300

    feat(routes): add routes configuration for main application and lazy load Cart component

---
+++ src/routes/routesConfig.tsx
@@ -0,0 +1,36 @@
+import App from "@/App";
+import CreateAccount from "@/pages/CreateAccount";
+import Creator from "@/pages/Creator";
+import LoginPage from "@/pages/login";
+import React, { Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+const Cart = React.lazy(() => import("../pages/cart"));
+
+const router = createBrowserRouter([
+  {
+    path: "/cart",
+    element: (
+      <Suspense fallback={<div>Carregando...</div>}>
+        <Cart />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/creator",
+    element: <Creator/>,
+  },
+  {
+    path: "/createAccount",
+    element: <CreateAccount />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
+export default router

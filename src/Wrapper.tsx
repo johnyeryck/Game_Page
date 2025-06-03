9c8f11f54d7af610a628c75673d09014f531bab6
johnyeryck <johnybombado@gmail.com>
2025-06-02 21:52:04 -0300

    feat(Wrapper): add Wrapper component for context provider and routing

---
+++ src/Wrapper.tsx
@@ -0,0 +1,28 @@
+import { RouterProvider } from "react-router-dom"
+import MyContext from "./context/gamesContext"
+import DataGames from "./interfaces"
+import { useQuery } from "@tanstack/react-query"
+import router from "./routes/routesConfig"
+
+function Wrapper(){
+    const { data} = useQuery<DataGames[] | undefined>({
+      
+    queryKey: ['games'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3000/games', {
+        method : "GET"
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      
+      return response.json()
+    }
+  })
+  return(
+        <MyContext.Provider value={data}>
+            <RouterProvider router={router}/>
+        </MyContext.Provider>
+  )
+}
+export default Wrapper

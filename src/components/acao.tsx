eb1b8d221dd0428c9167ba25eef71feb57d12e8d
johnyeryck <johnybombado@gmail.com>
2025-06-02 21:52:24 -0300

    fix(Acao): correct localStorage key case and update Options component prop

--- src/components/acao.tsx
+++ src/components/acao.tsx
@@ -3,7 +3,7 @@ import MyContext from "@/context/gamesContext"
 import { useContext } from "react"
 function Layout (){
     const data = useContext(MyContext)
-    console.log(localStorage.getItem("idGameCart"))
+    console.log( typeof localStorage.getItem("idgame"))
     return (
         <main className="ml-auto mr-auto w-[80%] ">
           <h5 className="text-white pl-4 mb-6 font-extrabold">Jogos De Tirar O fôlego</h5>
@@ -14,7 +14,7 @@ function Layout (){
                   data.map((game)=>(
                     game.categoria === "Ação" ? (
                         <div className="ml-3 " key={game.id}>
-                            <Options  imageUrl={game.imagem_url} gameID={game.id}/>
+                            <Options  imageUrl={game.imagem_url} gameName={game.titulo}/>
                             <p className="text-white font-bold mt-6 ml-2 " >{game.titulo}</p>
                             <p className="text-white text-left font-bold ml-3 " >{game.preco}</p>
                         </div>

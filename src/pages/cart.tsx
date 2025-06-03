aa78f8a8ce6789dd7dcdc749591a76e713b0499a
johnyeryck <johnybombado@gmail.com>
2025-06-02 21:52:50 -0300

    fix(Cart): refactor Cart component to use context for game data and simplify rendering logic

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,20 +1,32 @@
 import Nav from "@/components/Nav";
 import Options from "@/components/options";
+import MyContext from "@/context/gamesContext";
+import { useContext } from "react";
 
 function Cart() {
-
   let isLoged = localStorage.getItem("isLoged")
+  const gameStorage = localStorage.getItem("idgame")
+  const data = useContext(MyContext)
   return (
       
-    <div>
+    <main>
       <Nav/>
       {
-        isLoged == "true" ? (
-          <Options imageUrl="" />
+          data && (
+            data.map((game)=>(
+              game.titulo === gameStorage ? (
+              <div className="" >
+                <Options imageUrl={game.imagem_url} />
+                <h1 className="text-white">{game.titulo}</h1>
+              </div>
+  
+              ):null
 
-        ) : null
+          )
+
+          )) 
       }
-    </div>
+    </main>
       
     
   )

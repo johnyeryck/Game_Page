3d4311fde8967645765bc70657e32964cf00c456
johnyeryck <johnybombado@gmail.com>
2025-06-02 21:51:47 -0300

    fix(main): simplify main component by removing unused routes and imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import React from "react";
-import { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Creator from "./pages/Creator.tsx";
-import CreateAccount from "./pages/CreateAccount.tsx";
-import LoginPage from "./pages/login.tsx";
+import Wrapper from "./Wrapper.tsx";
+
 const queryClient = new QueryClient();
 
-const Cart = React.lazy(() => import("./pages/cart.tsx"));
-const router = createBrowserRouter([
-  {
-    path: "/cart",
-    element: (
-      <Suspense fallback={<div>Carregando...</div>}>
-        <Cart />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/creator",
-    element: <Creator />,
-  },
-  {
-    path: "/createAccount",
-    element: <CreateAccount />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-]);
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Wrapper />
     </QueryClientProvider>
   </StrictMode>,
 );

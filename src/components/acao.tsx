13d7fea1f5d9cd6c83c31efc70b1f62e5164d3a6
johnyeryck <johnybombado@gmail.com>
2025-06-05 22:51:15 -0300

    fix(layout): adjust layout and improve rendering of action games in the component

--- src/components/acao.tsx
+++ src/components/acao.tsx
@@ -3,9 +3,9 @@ import MyContext from "@/context/gamesContext"
 import { useContext } from "react"
 function Layout (){
     const data = useContext(MyContext)
-    console.log( typeof localStorage.getItem("idgame"))
+
     return (
-        <main className="ml-auto mr-auto w-[80%] ">
+        <main className="ml-auto mr-auto w-[76%] ">
           <h5 className="text-white pl-4 mb-6 font-extrabold">Jogos De Tirar O fôlego</h5>
               
              <section className="flex justify-evenly">
@@ -13,10 +13,15 @@ function Layout (){
                 data &&  (
                   data.map((game)=>(
                     game.categoria === "Ação" ? (
+                      
                         <div className="ml-3 " key={game.id}>
-                            <Options  imageUrl={game.imagem_url} gameName={game.titulo}/>
-                            <p className="text-white font-bold mt-6 ml-2 " >{game.titulo}</p>
-                            <p className="text-white text-left font-bold ml-3 " >{game.preco}</p>
+                            <Options 
+                             imageUrl={game.imagem_url} game={game.titulo} gameid={game.id}/>
+                            <div className=" h-16 ">
+                                <p className="text-white font-bold mt-2 ml-2 " >{game.titulo}</p>
+                            </div>
+                            <p className="text-white text-left font-medium ml-2 " >{game.preco}</p>
+
                         </div>
                     ): null
                   ))

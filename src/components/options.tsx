229f21c8c98703aedf6215334c41e92984d6411c
johnyeryck <johnybombado@gmail.com>
2025-06-05 22:51:47 -0300

    fix(options): refactor CartInterface and update CartFunction to use IndexedDB for storing game data

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -1,18 +1,30 @@
 import Carrinho from "../assets/logo/carrinho-de-compras.png";
 import { FunctionComponent } from "react";
+import {openDB} from 'idb'
+import { dbPromise } from "@/indexDb";
 interface CartInterface{
   imageUrl : string | undefined,
-  gameName? : string | undefined,
+  game?: string,
+  isInvisible? : boolean,
+  gameid? : number 
 }
-const  Options : FunctionComponent<CartInterface>=({imageUrl , gameName})=> {
-      const CartFunction = ()=>{
-        alert("adicionado ao carinho")
-        localStorage.setItem("idgame" , JSON.stringify(gameName))
-      }
+const Options : FunctionComponent<CartInterface>=({imageUrl ,game, isInvisible , gameid })=> {
+  
+  const CartFunction = async()=>{
+    alert("adicionado ao carinho")
+    const db = await dbPromise
+    await db.put('jogos',{
+      id : gameid,
+      gamename : game
+    })
+  }
+      
+
+
       return(
           <div className="h-60  w-48 transform transition-all hover:scale-110 rounded-xl  ">
               <img className=" rounded-2xl size-full" src={imageUrl}/>
-              <button className="absolute bottom-4 right-4 bg-white p-2 rounded-full size-8 cursor-pointer"  onClick={()=>CartFunction()} ><img src={Carrinho} alt="Adicionar ao carrinho" /></button>
+              <button className="absolute bottom-4 right-4 bg-white p-2 rounded-full size-8 cursor-pointer "  hidden={isInvisible}  onClick={()=>CartFunction() } ><img src={Carrinho} alt="Adicionar ao carrinho" /></button>
           </div>
       )
 }

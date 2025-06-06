e0c5cfc458b340048848e491e7fc9cca2a759e0b
johnyeryck <johnybombado@gmail.com>
2025-06-05 22:51:56 -0300

    fix(rpg): adjust layout and formatting of RPG component for improved readability

--- src/components/rpg.tsx
+++ src/components/rpg.tsx
@@ -4,20 +4,22 @@ import Options from "./options"
 
 function Rpg (){
     const data = useContext(MyContext)
-    console.log(data)
     return(
     <main className=" mt-60">
         <h5 className="text-white font-extrabold ml-40 ">Historias Incríveis para se aventurar</h5>
-        <section className="flex w-[80%] ml-auto mr-auto justify-evenly">
+        <section className="flex w-[76%] ml-auto mr-auto justify-evenly">
 
                 { data && data.length > 0 ?(
                 data.map((game)=>(
                     game.categoria === "RPG" ?(
                         
-                        <div className="ml-3 " key={game.id}>
-                            <Options  imageUrl={game.imagem_url}  gameID={game.id}/>
-                            <p className="text-white font-bold mt-6 ml-2 " >{game.titulo}</p>
-                            <p className="text-white text-left font-bold ml-3 " >{game.preco}</p>
+                        <div className="ml-3   " key={game.id}>
+                            <Options  imageUrl={game.imagem_url} game={game.titulo}/>
+                                <div className=" h-16 ">
+                                    <p className="text-white font-bold mt-2 ml-2 " >{game.titulo}</p>
+                                </div>
+                            <p className="text-white text-left font-medium ml-2 " >{game.preco}</p>
+
                         </div>
                         ) : null
                     ))

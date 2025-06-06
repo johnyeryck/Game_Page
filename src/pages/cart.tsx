4dae7a21cd764462b67805ce0ac669e8519b79d7
johnyeryck <johnybombado@gmail.com>
2025-06-05 22:52:02 -0300

    fix(cart): refactor Cart component to fetch game data from IndexedDB and improve layout

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,32 +1,56 @@
 import Nav from "@/components/Nav";
 import Options from "@/components/options";
 import MyContext from "@/context/gamesContext";
-import { useContext } from "react";
+import { dbPromise } from "@/indexDb";
+import { useContext, useEffect, useState } from "react";
 
-function Cart() {
+ function Cart() {
+  interface jogoInterface {
+    id? : number,
+    gamename? : string
+  }
+
   let isLoged = localStorage.getItem("isLoged")
-  const gameStorage = localStorage.getItem("idgame")
+  const [jogo , setjogo] = useState<jogoInterface[]>()
   const data = useContext(MyContext)
+  useEffect(()=>{
+   const fetchIDB = async()=>{
+    const dbCall = await dbPromise
+    const id= await dbCall.getAll('jogos')
+    setjogo(id)
+    console.log(id)
+    }
+    fetchIDB()
+  },[])
+
+  console.log(jogo)
   return (
       
-    <main>
+    <header>
       <Nav/>
+       <main className="mt-40 mb-20">
+
       {
-          data && (
-            data.map((game)=>(
-              game.titulo === gameStorage ? (
-              <div className="" >
-                <Options imageUrl={game.imagem_url} />
-                <h1 className="text-white">{game.titulo}</h1>
+        data && jogo && (
+          data.map((game)=>(
+             jogo.find((p) => p.id == game.id)?(
+              <div className="border-2 rounded-lg w-2/4 mr-auto flex ml-10 border-purple-700 mt-10" >
+                <Options imageUrl={game.imagem_url} isInvisible={true}/>
+                <div className="flex-col">
+                    <h1 className="text-white font-medium mt-2 ml-3">{game.titulo}</h1>
+                    <p className="text-white mt-20 ml-3">{game.preco}</p>
+                    
+                </div>
               </div>
   
               ):null
 
           )
 
-          )) 
-      }
-    </main>
+        ))
+       }
+          </main>
+    </header>
       
     
   )

a87a8bae577828835778ffc1ce120d42379d9221
johnyeryck <johnybombado@gmail.com>
2025-06-08 09:49:41 -0300

    fix(cart): refactor game display structure and enhance remove functionality

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,6 +3,7 @@ import Options from "@/components/options";
 import MyContext from "@/context/gamesContext";
 import { dbPromise } from "@/indexDb";
 import { useContext, useEffect, useState } from "react";
+import Dumpster from '../assets/logo/excluir.png'
 
  function Cart() {
   interface jogoInterface {
@@ -12,7 +13,9 @@ import { useContext, useEffect, useState } from "react";
 
   let isLoged = localStorage.getItem("isLoged")
   const [jogo , setjogo] = useState<jogoInterface[]>()
+
   const data = useContext(MyContext)
+
   useEffect(()=>{
    const fetchIDB = async()=>{
     const dbCall = await dbPromise
@@ -23,31 +26,48 @@ import { useContext, useEffect, useState } from "react";
     fetchIDB()
   },[])
 
-  console.log(jogo)
+
+ const Remove = async (game: any)=>{
+   const db = await dbPromise
+  await  db.delete("jogos" , game)
+  window.location.reload();
+
+ }
+
+
+  
   return (
       
     <header>
       <Nav/>
-       <main className="mt-40 mb-20">
+       <main className="mt-40 mb-20 ">
 
       {
-        data && jogo && (
+        data && jogo ? (
           data.map((game)=>(
              jogo.find((p) => p.id == game.id)?(
-              <div className="border-2 rounded-lg w-2/4 mr-auto flex ml-10 border-purple-700 mt-10" >
-                <Options imageUrl={game.imagem_url} isInvisible={true}/>
-                <div className="flex-col">
-                    <h1 className="text-white font-medium mt-2 ml-3">{game.titulo}</h1>
-                    <p className="text-white mt-20 ml-3">{game.preco}</p>
-                    
-                </div>
-              </div>
+
+               <section className=" rounded-lg w-2/4 mr-auto flex ml-auto mt-10 "  >
+                    <div className="border rounded-2xl shadow-2xl h-61 shadow-purple-600">
+                       <Options imageUrl={game.imagem_url} isInvisible={true}/>
+
+                    </div>
+
+                    <div className="flex-col border-b border-r border-t rounded-r-sm border-purple-600 w-full ">
+                      <div className="flex ">
+                        <h1 className="text-white font-medium mt-2 ml-3">{game.titulo}</h1>
+                        <button className="size-7 ml-auto mr-2 mt-2 cursor-pointer" onClick={()=> Remove(game.id)}><img src={Dumpster} alt="" /></button>
+                      </div>
+                        <p className="text-white mt-20 ml-4">{game.preco}</p>
+                        <button className="bg-purple-800 cursor-pointer border-2 border-purple-600 mb-2 rounded-md text-white w-40 mt-18 ml-3 hover:bg-purple-600" >Finalizar Compra</button>
+                    </div>
+              </section>
   
-              ):null
+              ): null
 
           )
 
-        ))
+        )) : (<h1 className="text-white mt-40">teste</h1>)
        }
           </main>
     </header>

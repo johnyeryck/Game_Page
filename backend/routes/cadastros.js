48d70798da7540b547f874b700fbc5e116072d69
johnyeryck <johnybombado@gmail.com>
2025-06-21 01:33:09 -0300

    feat(cadastro): import email verification controller for user registration

--- backend/routes/cadastros.js
+++ backend/routes/cadastros.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import createUser from "../controllers/cadastroController.js";
+import emailVerification from "../controllers/verifyEmail.js";
 const router = Router();
 
 router.post("/", createUser);

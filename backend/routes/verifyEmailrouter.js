e39da6e033768deac1b8db07166905d746e0a412
johnyeryck <johnybombado@gmail.com>
2025-06-21 01:33:14 -0300

    feat(verifyEmail): create router for email verification endpoint

---
+++ backend/routes/verifyEmailrouter.js
@@ -0,0 +1,6 @@
+import Router from "express";
+import emailVerification from "../controllers/verifyEmail.js";
+const router = Router();
+// router.post("/", emailVerification);
+
+export default router;

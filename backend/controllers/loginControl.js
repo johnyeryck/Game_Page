421f973e5646d3686e083360e8464c7a29b12d81
johnyeryck <johnybombado@gmail.com>
2025-06-27 19:53:47 -0300

    refactor(loginControl): remove unnecessary POST method check

--- backend/controllers/loginControl.js
+++ backend/controllers/loginControl.js
@@ -2,7 +2,6 @@ import pool from "../db.js";
 
 const loginSys = async (req, res) => {
   const { email, senha } = req.body;
-  if (req.method === "POST") {
     const result = await pool.query(
       "SELECT * FROM usuarios WHERE email = $1 AND senhar = $2",
       [email, senha],
@@ -15,6 +14,5 @@ const loginSys = async (req, res) => {
       });
     }
   }
-};
 
 export default loginSys;

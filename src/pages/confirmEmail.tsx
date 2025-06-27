d0c2a1b377d01649f39a9edecc0416b44740339a
johnyeryck <johnybombado@gmail.com>
2025-06-27 19:55:30 -0300

    fix(confirmEmail): add toast notifications for email confirmation success

--- src/pages/confirmEmail.tsx
+++ src/pages/confirmEmail.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 const ConfirmEmail = ()=>{
     const [message, setMessage] = useState<boolean>()
@@ -9,6 +10,7 @@ const ConfirmEmail = ()=>{
            await fetch(`http://localhost:3000/confirmar?token=${token}` , {
             method : "POST"
            })
+           toast.success("E-mail Confirmado")
             window.location.href = "http://localhost:5173/login";
 
          }
@@ -16,8 +18,8 @@ const ConfirmEmail = ()=>{
  })
     return(
         <>
+        <ToastContainer />
         <div className="w-1/5 h-44 border ml-auto mr-auto">
-
         </div>
         </>
     )

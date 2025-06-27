c83c370a15a2add09ab0ed6a68af8643cbc7c6b4
johnyeryck <johnybombado@gmail.com>
2025-06-27 19:53:58 -0300

    refactor(cadastros): add rate limiting to user creation route

--- backend/routes/cadastros.js
+++ backend/routes/cadastros.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import createUser from "../controllers/cadastroController.js";
 import emailVerification from "../controllers/verifyEmail.js";
+import ratelimit from 'express-rate-limit'
+const limit = ratelimit({
+  windowMs : 1 * 60 * 1000,
+  max : 1,
+  message: "Espere"
+ })
 const router = Router();
 
-router.post("/", createUser);
+router.post("/",limit,createUser);
 
 export default router;

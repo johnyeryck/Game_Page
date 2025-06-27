1b4471d2c403c590435d2a4af3a80fe16d799ddb
johnyeryck <johnybombado@gmail.com>
2025-06-27 19:55:16 -0300

    fix(slide): enhance styling for SwiperSlide and improve visual consistency

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -29,20 +29,21 @@ function Main() {
           modifier: 1,
           slideShadows: true,
         }}
+        
       >
         {gamesApi.map((img, i) => {
           return (
             <SwiperSlide
-              className="transform transition-all   mt-14 mb-10"
+              className="mt-14 mb-10 border"
               key={i}
             >
-              <div className="border-2 border-purple-600 w-2/3 ml-auto mr-auto rounded-xl hover:brightness-90 relative ">
+              <div className="border-2 shadow-2xl  shadow-purple-300 border-purple-600 w-2/3 ml-auto mr-auto rounded-xl hover:brightness-90 relative ">
                 <img
                   src={img.img}
                   className="  rounded-xl ml-auto mr-auto  size-full "
                 ></img>
                 <div className="bg-gradient-to-r w-3/4  from-black to-transparent rounded-xl  absolute top-0 h-full ">
-                  <p className="text-white  leading-6 font-bold   w-sm  ml-7 mb-20 mt-72">
+                  <p className="text-white  leading-6 font-bold   w-sm  ml-7 mb-20 mt-72" >
                     {img.descricao}
                   </p>
                   <div className="absolute bottom-4 ">

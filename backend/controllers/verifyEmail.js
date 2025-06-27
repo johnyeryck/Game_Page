bfad11e4a3cfd7cb2b6f7840f8f6cf493d47d07b
johnyeryck <johnybombado@gmail.com>
2025-06-27 19:53:52 -0300

    refactor(verifyEmail): streamline email verification logic and improve error handling

--- backend/controllers/verifyEmail.js
+++ backend/controllers/verifyEmail.js
@@ -1,18 +1,19 @@
 import pool from "../db.js";
 import jwt from 'jsonwebtoken'
 const emailVerification = async (req ,res)=>{
-        try{
-                const { token } = req.query;
-                const decode = jwt.decode(token , 'segredo')
-                const { email , senha, user} = decode;
-                console.log(email)
-                await pool.query(
-                        "INSERT INTO usuarios (email,senhar,usuario) VALUES ($1,$2,$3) RETURNING *",
-                        [email, senha, user])
-        }catch{
-
+       
+        const { token } = req.query;
+        try {
+        const decode = jwt.decode(token, process.env.KEY_TOKEN);
+        const { email, senha, user } = decode;
+        await pool.query(
+        "INSERT INTO usuarios (email,senhar,usuario) VALUES ($1,$2,$3) RETURNING *",
+        [email, senha, user],
+        );
+        res.status(200).send("Usuario cadastrado")
+        } catch{
+        
         }
-    
 }
 
 export default emailVerification;

6d0885c102f42980f7a4d2daad65253ac39dc10b
johnyeryck <johnybombado@gmail.com>
2025-07-05 13:16:28 -0300

    fix(verifyEmail): add result logging after user registration

--- backend/controllers/verifyEmail.js
+++ backend/controllers/verifyEmail.js
@@ -4,13 +4,14 @@ const emailVerification = async (req ,res)=>{
        
         const { token } = req.query;
         try {
-        const decode = jwt.decode(token, process.env.KEY_TOKEN);
-        const { email, senha, user } = decode;
-        await pool.query(
-        "INSERT INTO usuarios (email,senhar,usuario) VALUES ($1,$2,$3) RETURNING *",
-        [email, senha, user],
-        );
+                const decode = jwt.decode(token, process.env.KEY_TOKEN);
+                const { email, senha, user } = decode;
+                 const result =  await pool.query(
+                "INSERT INTO usuarios (email,senhar,usuario) VALUES ($1,$2,$3) RETURNING *",
+                [email, senha, user],
+                );
         res.status(200).send("Usuario cadastrado")
+        console.log(result.rows)
         } catch{
         
         }

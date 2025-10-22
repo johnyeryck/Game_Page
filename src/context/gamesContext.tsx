085cfff82c39d9784d3559ff3b1e4e898e657625
johnyeryck <johnybombado@gmail.com>
2025-10-22 00:29:25 -0300

    ✨ chore(root): restructure project into monorepo
    - move backend and frontend into separate directories
    - update package.json to reflect monorepo structure
    - remove unused assets and components
    - update scripts to run backend and frontend servers separately

--- src/context/gamesContext.tsx
+++
@@ -1,6 +0,0 @@
-import DataGames from "@/interfaces";
-import { createContext } from "react";
-type GamesContextType = DataGames[] | undefined;
-const MyContext = createContext<GamesContextType>(undefined);
-
-export default MyContext;

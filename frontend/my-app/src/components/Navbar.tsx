f89ab5c94276fa533b3c88ff343e36fe58e90d90
johnyeryck <johnybombado@gmail.com>
2025-10-29 19:44:40 -0300

    ✨ feat(navbar): enhance navbar with search and styling
    - add search input for better user experience
    - implement shadow and z-index for improved visibility
    - adjust width of logo and cart sections for better layout

--- frontend/my-app/src/components/Navbar.tsx
+++ frontend/my-app/src/components/Navbar.tsx
@@ -5,17 +5,22 @@ import Carrinho from "@public/carrinho.png";
 // import HoverPerfil from "./hoverPerfil";
 function Nav() {
   return (
-    <header className="w-full bg-black h-20 flex  top-0 fixed ">
+    <header className="w-full bg-black h-20 flex  top-0 fixed z-10 shadow-2xl shadow-black">
       <nav className="flex w-full h-1/2  m-auto">
-        <Link href="/" className="flex  pl-2 cursor-pointer w-1/5">
+        <Link href="/" className="flex  pl-2 cursor-pointer w-1/6">
           <Image className="" src={Logo} alt="Gamers Logo" />
         </Link>
-        <article className="flex  ml-auto w-[15%]">
+        <input
+          type="search"
+          className="bg-[#0D0E1C] px-3 rounded-2xl mx-auto border-purple-800 border w-1/3"
+          placeholder="Search"
+        />
+        <div className="flex  ml-auto w-[15%]">
           <Link href="/cart" className=" w-1/5">
             <Image className="" src={Carrinho} alt="Gamers Logo" />
           </Link>
           {/* <HoverPerfil /> */}
-        </article>
+        </div>
       </nav>
     </header>
   );

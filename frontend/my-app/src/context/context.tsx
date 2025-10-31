77897bcc17240b94da667b26a1ee60168c268402
johnyeryck <johnybombado@gmail.com>
2025-10-31 00:56:21 -0300

    ♻️ refactor(context): rename preco to valor
    - rename preco property to valor in dataInterface

--- frontend/my-app/src/context/context.tsx
+++ frontend/my-app/src/context/context.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 export interface dataInterface {
   id: number;
   titulo: string;
-  preco: string;
+  valor: string;
   imagem_url: string;
   fullhd_img: string;
   logo: string;

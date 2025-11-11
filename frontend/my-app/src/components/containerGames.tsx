f3ca5c59ebb5cfcc6d0906a2794c18562233d725
johnyeryck <johnybombado@gmail.com>
2025-11-11 12:09:39 -0300

    ✨ feat(containerGames): add swiper navigation and title
    - add navigation arrows for improved user experience
    - implement dynamic title rendering for container sections
    - use tags instead of categoria

--- frontend/my-app/src/components/containerGames.tsx
+++ frontend/my-app/src/components/containerGames.tsx
@@ -1,28 +1,40 @@
 import { Mycontext } from "@/context/context";
 import Game from "@components/game";
 import { useContext } from "react";
-import { Navigation } from "swiper/modules";
+import { GoChevronRight, GoChevronLeft } from "react-icons/go";
+
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 interface containerProps {
   categoria?: "RPG" | "Ação" | "Luta";
+  ContainerTitle?: string;
 }
-function Layout({ categoria }: containerProps) {
+function Layout({ categoria, ContainerTitle }: containerProps) {
   const data = useContext(Mycontext);
-
   return (
     <header className=" mt-20 ">
       <Swiper slidesPerView={5} className=" w-[80%] ">
+        <div className="px-4 w-full flex justify-center absolute top-1 right-0 ">
+          <h2 className="mr-auto my-auto">{ContainerTitle}</h2>
+          <button className=" cursor-pointer bg-purple-950 size-8 rounded-full ">
+            <GoChevronLeft className="m-auto" size="50%" />
+          </button>
+          <button className="cursor-pointer size-8 rounded-full  bg-purple-950  ml-1">
+            <GoChevronRight size="50%" className="m-auto" />
+          </button>
+        </div>
         {data &&
           data.map((game) =>
-            game.categoria === categoria ? (
-              <SwiperSlide key={game.id} className="mt-2 ">
-                <article className=" cursor-pointer    ">
+            game.tags[0] === categoria ? (
+              <SwiperSlide key={game.id} className="mt-10 ">
+                <article className=" cursor-pointer   ">
                   <Game
                     key={game.id}
-                    imageUrl={game.imagem_url}
+                    imageUrl={game.statics.imagem_url}
                     name={game.titulo}
                     gameid={game.id}
                     height="240px"

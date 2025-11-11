5ae09dbd411a9dfac0598f441b370f65964ef0fb
johnyeryck <johnybombado@gmail.com>
2025-11-11 12:07:43 -0300

    ✨ feat(search): implement dynamic game search with slugified links
    - add search functionality to the navbar
    - filter games based on user input
    - display search results with image and title
    - create slugified links for each game in the search results
    - add HoverPerfil component

--- frontend/my-app/src/components/Navbar.tsx
+++ frontend/my-app/src/components/Navbar.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "@public/logo2.png";
 import Carrinho from "@public/carrinho.png";
-import { dataInterface, Mycontext } from "@/context/context";
+import { Mycontext } from "@/context/context";
+
 import { useContext, useState } from "react";
 import Game from "@components/game";
+import slugify from "@/slugify";
+import HoverPerfil from "./hoverPerfil";
 // import HoverPerfil from "./hoverPerfil";
 function Nav() {
   const [res, setRes] = useState<
-    { titulo: string; imagem_url: string; id: number }[] | undefined
+    | { titulo: string; statics: { imagem_url: string }; id: number }[]
+    | undefined
   >();
   const data = useContext(Mycontext);
   const searchGames = (e: any) => {
@@ -19,13 +23,13 @@ function Nav() {
     const list = data?.filter((item) => {
       const verify = item.titulo.toLocaleLowerCase().includes(tituloLower);
       if (value.length > 0) {
-        if (verify) return [item.titulo, item.imagem_url, item.valor, item.id];
+        if (verify)
+          return [item.titulo, item.statics.imagem_url, item.valor, item.id];
       }
     });
     const limit = list?.splice(0, 5);
 
     setRes(limit);
-    console.log(res);
   };
   return (
     <header className="w-full bg-black h-20 flex  top-0 fixed z-10 shadow-2xl shadow-black">
@@ -37,32 +41,33 @@ function Nav() {
           <input
             type="search"
             onChange={searchGames}
-            className="bg-[#0D0E1C] px-3 rounded-2xl mx-auto border-purple-800 border size-full"
+            className="bg-[#0D0E1C] outline-0  px-3 rounded-2xl mx-auto border-purple-800 border size-full"
             placeholder="Search"
           />
           <section className="backdrop-blur-lg rounded-2xl mt-2">
             {res &&
               res.map((item) => (
-                <div
+                <Link
+                  href={`/game/${slugify(item.titulo)}/${item.id}`}
                   className="pl-4 h-20 flex   pt-2 hover:border from-black to-transparent rounded-2xl  cursor-pointer"
                   key={item.id}
                 >
                   <Game
-                    imageUrl={item.imagem_url}
+                    imageUrl={item.statics.imagem_url}
                     height="60px"
                     width="50px"
-                    isInvisible
+                    HiddenCart
                   />
                   <p className="pl-2 my-auto">{item.titulo}</p>
-                </div>
+                </Link>
               ))}
           </section>
         </div>
         <div className="flex  ml-auto w-[15%]">
-          <Link href="/cart" className=" w-1/5">
+          <Link href="/cart" className=" w-1/5 mr-2">
             <Image className="" src={Carrinho} alt="Gamers Logo" />
           </Link>
-          {/* <HoverPerfil /> */}
+          <HoverPerfil />
         </div>
       </nav>
     </header>

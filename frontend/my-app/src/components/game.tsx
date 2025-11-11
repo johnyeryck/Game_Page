64d559fe08508b7ac1f67d6a4d85a4ef13907842
johnyeryck <johnybombado@gmail.com>
2025-11-11 12:09:10 -0300

    ✨ feat(game): implement indexedDB cart functionality
    - add indexedDB integration to store game data
    - implement `CartFunction` to save game name and ID
    - change `isInvisible` prop to `HiddenCart` to improve semantics

--- frontend/my-app/src/components/game.tsx
+++ frontend/my-app/src/components/game.tsx
@@ -4,10 +4,11 @@ import { FunctionComponent, useState } from "react";
 import slugify from "../slugify";
 import Link from "next/link";
 import Image, { StaticImageData } from "next/image";
+import { dbPromise } from "@/indexDb";
 interface CartInterface {
   imageUrl: string | Blob | undefined;
   name?: string;
-  isInvisible?: boolean;
+  HiddenCart?: boolean;
   gameid?: number;
   width?: string;
   height?: string;
@@ -15,20 +16,20 @@ interface CartInterface {
 const Options = ({
   imageUrl,
   name,
-  isInvisible,
+  HiddenCart,
   gameid,
   width,
   height,
 }: CartInterface) => {
   const CartFunction = async () => {
-    // const db = await dbPromise;
-    // await db.put("jogos", {
-    //   id: gameid,
-    //   gamename: game,
-    // });
+    const db = await dbPromise;
+    await db.put("jogos", {
+      id: gameid,
+      gamename: name,
+    });
   };
   return (
-    <Link href={`/game/${slugify(name!)}/${gameid}`}>
+    <Link href={`/game/${slugify(name || "")}/${gameid}`}>
       <div
         className="overflow-hidden scale-90  transform transition-all hover:brightness-50 rounded-xl"
         style={{ width: `${width}`, height: `${height}` }}
@@ -36,7 +37,7 @@ const Options = ({
         <img className="rounded-lg  size-full" src={imageUrl} alt="Jogo" />
         <button
           className="absolute bottom-4 right-4 bg-white p-2 rounded-full size-8 cursor-pointer border border-purple-600"
-          hidden={isInvisible}
+          hidden={HiddenCart}
           onClick={() => CartFunction()}
         >
           <Image src={Carrinho} alt="Adicionar ao carrinho" />

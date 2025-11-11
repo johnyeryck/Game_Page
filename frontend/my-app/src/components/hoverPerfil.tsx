8b8f14eac1d8879c12bf0bde22d3d5a15c6bb90f
johnyeryck <johnybombado@gmail.com>
2025-11-11 12:09:29 -0300

    ✨ feat(hoverPerfil): implement login/signup modal and user display
    - add login and signup components with modal display
    - display username and profile options after login
    - refactor component to handle user authentication state

--- frontend/my-app/src/components/hoverPerfil.tsx
+++ frontend/my-app/src/components/hoverPerfil.tsx
@@ -1,40 +1,66 @@
-import Usuario from "@/assets/logo/usuario.png";
-import Link from "next/link";
+import Usuario from "@public/icons/usuario.png";
 import Image from "next/image";
+import { useState } from "react";
+import Login from "@/components/Login";
+import Create from "@/components/SignUp";
 interface HoverPerfil {
-  UserisLoged?: boolean;
+  username?: string;
 }
-export default function HoverPerfil({}: HoverPerfil) {
+export default function HoverPerfil({ username }: HoverPerfil) {
+  const [login, setHidden] = useState<string | undefined>();
+
   return (
     <>
       <nav className="flex  ml-auto m-auto ">
-        <Link href="/login" className="flex mr-3 text-white ">
+        <button
+          onClick={() => setHidden("login")}
+          className="flex mr-3 text-white "
+        >
           Entrar
-        </Link>
-        <Link href="/criarconta" className="flex mr-3 text-white">
-          | Registrar
-        </Link>
+        </button>
+        <button
+          onClick={() => setHidden("create")}
+          className="flex mr-3 text-white "
+        >
+          Registrar
+        </button>
       </nav>
-      <div className="flex ">
-        <p className="text-white mt-12 font-bold">Username</p>
-        <Image
-          className="size-11 mt-9 cursor-pointer "
-          src={Usuario}
-          alt="Foto de usuario"
-        />
 
-        <ul className="absolute right-0 top-20 group-hover:block bg-gray-300 text-black shadow-lg rounded-md w-24 font-semibold">
-          <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">
-            Perfil
-          </li>
-          <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer ">
-            Config
-          </li>
-          <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">
-            Sair
-          </li>
-        </ul>
-      </div>
+      {login === "login" ? (
+        <Login
+          onClick={() => setHidden("")}
+          SignUp={() => setHidden("create")}
+        />
+      ) : null}
+      {login === "create" ? (
+        <Create
+          onClick={() => setHidden("")}
+          Signin={() => setHidden("login")}
+        />
+      ) : null}
+
+      {username && (
+        <div className="flex ">
+          <p className="text-white mt-12 font-bold">{username}</p>
+          <Image
+            className="size-11 mt-9 cursor-pointer "
+            src={Usuario}
+            alt="Foto de usuario"
+          />
+
+          <ul className="absolute right-0 top-20 group-hover:block bg-gray-300 text-black shadow-lg rounded-md w-24 font-semibold">
+            <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">
+              Perfil
+            </li>
+            <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer ">
+              Config
+            </li>
+            <li className="py-2 px-4 hover:bg-purple-600 hover:rounded-md cursor-pointer">
+              Sair
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }

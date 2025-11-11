bf9c6f7f73b33dc9e0e7f11ab9eb527d9b4b23e0
johnyeryck <johnybombado@gmail.com>
2025-11-11 12:08:45 -0300

    ✨ feat(slide): implement dynamic slider with game data
    - integrate game data from context into slider
    - filter games based on provided ids
    - improve slider ui with dynamic images and buy button

--- frontend/my-app/src/components/slide.tsx
+++ frontend/my-app/src/components/slide.tsx
@@ -1,7 +1,8 @@
 import { SwiperSlide, Swiper } from "swiper/react";
 import { useContext } from "react";
 interface jogoint {
-  name: string;
+  name?: string;
+  id?: number[];
 }
 import {
   Autoplay,
@@ -10,13 +11,13 @@ import {
   Pagination,
 } from "swiper/modules";
 import { Mycontext } from "@/context/context";
-function Slide() {
-  const destaques: jogoint[] = [
-    { name: "Grand Theft Auto VI" },
-    { name: "Ghost of Tsushima Director's Cut" },
-    { name: "Cyberpunk 2077" },
-    { name: " DRAGON BALL: Sparking! ZERO" },
-  ];
+function Slide({ id }: jogoint) {
+  // const destaques: jogoint[] = [
+  //   { name: "Grand Theft Auto VI" },
+  //   { name: "Ghost of Tsushima Director's Cut" },
+  //   { name: "Cyberpunk 2077" },
+  //   { name: " DRAGON BALL: Sparking! ZERO" },
+  // ];
   const data = useContext(Mycontext);
   return (
     <header className=" ">
@@ -39,22 +40,19 @@ function Slide() {
           slideShadows: true,
         }}
       >
-        {data && data.length > 0
+        {data && id && data.length > 0
           ? data.map((game) =>
-              destaques.find((p) => p.name == game.titulo) ? (
-                <SwiperSlide className="mt-14 mb-10 ">
-                  <main className=" w-2/3 ml-auto border-2 border-purple-600 mr-auto rounded-2xl hover:brightness-90 relative ">
+              id.find((item) => item === game.id) ? (
+                <SwiperSlide className="mt-14 mb-10">
+                  <main className=" w-2/3 mx-auto   overflow-hidden rounded-2xl hover:brightness-90 relative ">
                     <div className="absolute w-1/2 h-full rounded-2xl bg-linear-to-r from-purple-600 to-transparent "></div>
                     <div className="absolute right-0 w-full h-full rounded-2xl bg-linear-to-l  from-black to-transparent "></div>
+                    <img src={game.statics.fullhd_img} className=" size-full" />
                     <img
-                      src={game.fullhd_img}
-                      className="  rounded-xl ml-auto mr-auto  size-full "
+                      src={game.statics.logo}
+                      className="absolute bottom-1/3 ml-4 w-[20%] "
                     />
-                    <img
-                      src={game.logo}
-                      className="absolute bottom-1/3 ml-4 w-56 "
-                    />
-                    <button className=" font-bold  bg-white border border-black text-black shadow-2xl inset-shadow-amber-700 bottom-4 cursor-pointer  absolute ml-6  px-6 py-2 rounded-2xl   transition-colors duration-300 ">
+                    <button className=" font-bold  bg-white border border-black text-black shadow-2xl inset-shadow-amber-700 bottom-4 cursor-pointer  absolute ml-6  px-6 py-2 rounded-2xl   transition-colors duration-300 hover:bg-purple-600 hover:border-2 hover:border-white hover:shadow-2xl hover:shadow-purple-600 hover:text-white">
                       Comprar Agora
                     </button>
                   </main>

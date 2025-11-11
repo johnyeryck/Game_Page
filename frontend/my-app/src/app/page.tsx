c5cc887b292b43facf5c989947ef4a199dd8a8ab
johnyeryck <johnybombado@gmail.com>
2025-11-11 19:56:13 -0300

    refactor(page): replace local data fetching with react-query for improved data management

--- frontend/my-app/src/app/page.tsx
+++ frontend/my-app/src/app/page.tsx
@@ -1,57 +1,36 @@
 // app/page.tsx
 "use client";
-import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import { useQuery } from "@tanstack/react-query";
 import Navbar from "@/components/Navbar";
 import GameContainer from "@/components/containerGames";
 import { Mycontext } from "@/context/context";
-import { dataInterface } from "@/context/context";
 import Slide from "@/components/slide";
 export default function Home() {
-  const [data, setData] = useState<dataInterface[] | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("http://localhost:4000/", {
-          method: "GET",
-        });
+ const {data , isLoading , error} = useQuery({
+    queryKey  : ["Games"],
+    queryFn : ()=> fetch("http://localhost:4000").then((res)=> res.json())
+ })
 
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-
-        const result = await response.json();
-        setData(result);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchGames();
-  }, []);
-  if (isLoading) return <div>Carregando...</div>;
-  if (error) return <div>Erro: {error}</div>;
+ if(isLoading) return 'Loading ...'
+ if(error) return 'Algo deu errado'
 
   return (
     <>
-      <Mycontext.Provider value={data}>
         <header className="bg-linear-to-t from-black to-transparent">
-          <Navbar />
-          <Slide id={[10, 2, 37, 20, 30]} />
+          <Navbar data={data} />
+          <Slide id={[10, 2, 37, 20, 30]} data={data}/>
           <GameContainer
             categoria="Ação"
             ContainerTitle="Recomendados e Destaques"
+            data={data}
           />
           <GameContainer
             categoria="RPG"
+            data={data}
             ContainerTitle="Historias Para se Aventurar"
           />
         </header>
-      </Mycontext.Provider>
     </>
   );
 }
